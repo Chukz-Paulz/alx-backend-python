3be48a00a4440395af6ab8a4a905c2319123156e
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2024-10-11 00:25:41 +0000

    Python Variable Annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""The Module that contains a single function that adds two integers."""
+
+
+def add(a: float, b: float) -> float:
+    """ This add two integers together."""
+    return a + b
